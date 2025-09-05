d01641e649a0b806f6d12259b1351d7b1183bbbd
THIAGO DE ANDRADE MONTEIRO <tmonteiro651@gmail.com>
2025-09-04 21:53:42 -0300

    aula-javascipt

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-alert("Olá mundo!")
+// alert("olá mundo")
+// alert("Bem vindo a este site")
+  
+// const let idade
 
+// idade = 18
+
+// idade = "Nescau"
+// alert(idade)
+// idade = "dezoito"
+// alert(idade)
+
+
+// idade = "18.1"
+
+
+amor = "amorzinho"
+
+alert(amor)
+let amor2 = "vem ca amor"
+alert(amor2)
+
+var antiguado
