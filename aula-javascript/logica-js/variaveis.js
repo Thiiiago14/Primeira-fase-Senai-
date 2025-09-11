7ddbac91e1e3670a447fc7488ecf895c86159cb4
thiiiago14 <tmonteiro651@gmail.com>
2025-09-10 22:27:20 -0300

    sq02

---
+++ aula-javascript/logica-js/variaveis.js
@@ -0,0 +1,23 @@
+//variável com var
+//var nome = "thiago"
+// var nome = "joão"
+//nome = "joão"
+
+//variavel com let
+//let nome = "Thiago"
+//nome = "joão"
+//variavel declarada com const
+ const nome = "Thiago" //string
+ const sobrenome = "Monteiro"
+const idade = 25 //number
+const masculino = true //boolean
+
+//array
+ const frutas = ["laranja", "maçã", "banana"]
+
+console.log(frutas[1])
+
+// console.log("Meu nome é "+ nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
+
