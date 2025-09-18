30992fe7658241839391651bf8c2ada820794bae
thiiiago14 <tmonteiro651@gmail.com>
2025-09-17 22:18:24 -0300

    SQ02

---
+++ sq02/ex55.js
@@ -0,0 +1,36 @@
+let ponto
+
+ponto = Number(prompt("Digite sua pontuação: "))
+if(ponto <=10){
+    alert("Deu Ruim!")
+    alert("Vai estudar invés de ficar jogando")
+
+}else
+    if(ponto >= 10){
+        alert("é dá para melhorar")
+}else if
+    (ponto >= 100){
+        alert("Sumpimpa!!")
+
+}else if
+    (ponto >= 200){
+        alert("Mitou!")
+}
+console.log(ponto)
+
+// let ponto
+
+// ponto = Number(prompt("Digite sua pontuação: "))
+
+// if (ponto <= 10) {
+//     alert("Deu Ruim!")
+//     alert("Vai estudar invés de ficar jogando")
+// } else if (ponto > 10 && ponto <= 100) {
+//     alert("É, dá para melhorar")
+// } else if (ponto > 100 && ponto <= 200) {
+//     alert("Sumpimpa!!")
+// } else if (ponto > 200) {
+//     alert("Mitou!")
+// }
+
+// console.log(ponto)
