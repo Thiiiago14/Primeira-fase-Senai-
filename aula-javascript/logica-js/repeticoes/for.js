e10f1c065385182e51c50fc8cbdb8dc25a1b1c3a
thiiiago14 <tmonteiro651@gmail.com>
2025-10-02 21:26:51 -0300

    exercicios repetições

---
+++ aula-javascript/logica-js/repeticoes/for.js
@@ -0,0 +1,30 @@
+// let i = 0
+// while(i<3){
+// alert("valor do i : " + i)
+// i++ 
+
+// }
+// for(let i = 0; i<3; i++){
+
+// }
+// let soma = 0
+// let nota1 = Number(prompt("digite a nota: "))
+// soma = nota1
+// let nota2 = Number(prompt("digite a 2 nota: "))
+// soma = soma + nota2
+// let nota3 = Number(prompt("digite a 3 nota: "))
+// soma = soma + nota3
+// let soma = 0
+
+// for(let i=0; i<10; i++){
+//     let nota = Number(prompt("digite a "+ (i+1) + "a nota: " ))
+// soma  = soma + nota 
+
+// }
+// alert("soma: " + soma)
+
+const nomes = ["Dona bete", "Mano juca", "Lucio fernando", "capitão ganso"]
+// alert(nomes[0])
+
+for(let i=0; i<4; i++)
+    alert(nomes[i])
--- aula-javascript/logica-js/repeticoes/script.js
+++ aula-javascript/logica-js/repeticoes/script.js
@@ -6,7 +6,7 @@
 let contador = 0
 
 while(contador < 5){
-    //alert("oi")
+    //alert("contador:" + contador)
     contador = contador + 1
     console.log("contador:"+ contador);
     
