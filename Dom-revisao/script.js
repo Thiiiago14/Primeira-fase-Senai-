ea076fe90b704b83986a3bb36f1d532ceb01dfb0
thiiiago14 <tmonteiro651@gmail.com>
2025-10-27 21:37:07 -0300

    dom-revisao2

--- Dom-revisao/script.js
+++ Dom-revisao/script.js
@@ -11,4 +11,23 @@
 
 // }
 
+function calcularEx1014(){
+    let distancia = Number( document.getElementById('inputDistancia').value )
+    let combustivel = Number( document.getElementById('inputCombustivel').value )
 
+    let consumo = distancia / combustivel
+    console.log(consumo);
+
+    // document.getElementById('resultadoConsumo').innerHTML = "O consumo foi de " + consumo + "km/l"
+    document.getElementById('resultadoConsumo').innerHTML = `O consumo foi de ${consumo}km/l`
+    
+}
+
+
+function executarTeste(){
+    let nome = document.getElementById("inputNome").value
+    // console.log(nome);
+
+    document.getElementById('resultado').innerHTML += nome    
+    
+}
